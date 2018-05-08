577b899c0b815048ce979d1d2363e4b1ba73b58e
= <=>
2018-05-08 17:57:17 -0500

    colorizing stuff

--- parallelize.py
+++ parallelize.py
@@ -5,7 +5,27 @@ import subprocess
 import math
 from multiprocessing import Pool, Lock
 
+BLACK = "\033[0;30m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+YELLOW = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+WHITE  = "\033[0;37m"
+RESET = "\033[0;0m"
+
 global_lock = Lock()
+
+def colorize(s):
+    s = s.replace("[       OK ]", GREEN + "[       OK ]" + RESET) 
+    s = s.replace("[ RUN      ]", GREEN + "[ RUN      ]" + RESET) 
+    s = s.replace("[  PASSED  ]", GREEN + "[  PASSED  ]" + RESET) 
+    s = s.replace("[==========]", GREEN + "[==========]" + RESET) 
+    s = s.replace("[----------]", GREEN + "[----------]" + RESET) 
+    s = s.replace("[  FAILED  ]", RED + "[  FAILED  ]" + RESET) 
+    
+    return s
 
 def run_test(test_args):
     res = subprocess.Popen(test_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -13,39 +33,43 @@ def run_test(test_args):
 
     out_text = out.decode('utf-8').strip().split('\n')
 
+    # out_text = [colorize(s) for s in out_text]
+
     # This is the case when the test case dies early
     if len(out_text) < 8:
 
         global_lock.acquire()
-        print('|==================================================|')
-        print('|>>>FAILING TEST CASE DETECTED-- LIKELY SEGFAULT<<<|')
-        print('|==================================================|')
-        print('|>>>Printing out GTEST output followed by stderr<<<|')
-        print('|==================================================|')
-        print(out.decode('utf-8').strip())
-        print('\n▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲')
-        print('▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼\n')
-        print(err.decode('utf-8').strip())
-        print('\n▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲\n')
+        print(RED, '|==================================================|', RESET, sep='')
+        print(RED, '|>>>FAILING TEST CASE DETECTED-- LIKELY SEGFAULT<<<|', RESET, sep='')
+        print(RED, '|==================================================|', RESET, sep='')
+        print(RED, '|>>>Printing out GTEST output followed by stderr<<<|', RESET, sep='')
+        print(RED, '|==================================================|', RESET, sep='')
+        print(YELLOW, '▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼', RESET, sep='')
+        print(colorize(out.decode('utf-8').strip()))
+        print(YELLOW, '▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲', RESET, sep='')
+        print(YELLOW, '▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼', RESET, sep='')
+        print(colorize(err.decode('utf-8').strip()))
+        print(YELLOW, '▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲', RESET, sep='')
         global_lock.release()
 
         return out, err
     
     test_name = out_text[1][27:]
-    if out_text[7].startswith('[       OK ]'):
+    if out_text[7].startswith(GREEN + '[       OK ]'):
         return out, None
     else:
         global_lock.acquire()
-        print('|==================================================|')
-        print('|>>> FAILING TEST CASE DETECTED-- INVALID VALUE <<<|')
-        print('|==================================================|')
-        print('|>>>Printing out GTEST output followed by stderr<<<|')
-        print('|==================================================|')
-        print(out.decode('utf-8').strip())
-        print('\n▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲')
-        print('▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼\n')
-        print(err.decode('utf-8').strip())
-        print('\n▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲\n')
+        print(RED, '|==================================================|', RESET, sep='')
+        print(RED, '|>>> FAILING TEST CASE DETECTED-- INVALID VALUE <<<|', RESET, sep='')
+        print(RED, '|==================================================|', RESET, sep='')
+        print(RED, '|>>>Printing out GTEST output followed by stderr<<<|', RESET, sep='')
+        print(RED, '|==================================================|', RESET, sep='')
+        print(YELLOW, '▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼', RESET, sep='')
+        print(colorize(out.decode('utf-8').strip()))
+        print(YELLOW, '▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲', RESET, sep='')
+        print(YELLOW, '▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼', RESET, sep='')
+        print(colorize(err.decode('utf-8').strip()))
+        print(YELLOW, '▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲', RESET, sep='')
         global_lock.release()
         
         return out, err
@@ -90,8 +114,8 @@ for res in result:
     if res[1] != None:
         fails += 1
 
-output = '|>>>We ran ' + str(len(result)) + ' test cases, passing ' + str(len(result) - fails) + ' test case(s) and failing ' + str(fails) + ' test case(s)!<<<|'
-outer = '|' + ('=' * (len(output) - 2)) + '|'
+output = '|>>>' + YELLOW + 'We ran ' + str(len(result)) + ' test cases, ' + GREEN + 'passing ' + str(len(result) - fails) + ' test case(s) ' + RESET + 'and ' + RED + 'failing ' + str(fails) + ' test case(s)!' + RESET + '<<<|'
+outer = '|' + ('=' * (len(output) - 2 - 5 * 5)) + '|'
 
 global_lock.acquire()
 print(outer)
